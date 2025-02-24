8cc6ab43c46f285fe478a5843c63bdd0ff2dc366
jiwoolee2 <dlwldn1009@naver.com>
2025-02-24 18:00:26 +0900

    0224

--- General/Scripts/04_JOIN.sql
+++ General/Scripts/04_JOIN.sql
@@ -302,7 +302,21 @@ ORDER BY EMP_NAME, DEPT_CODE;
 
 -- '='(등호)를 사용하지 않는 조인문
 --  지정한 컬럼 값이 일치하는 경우가 아닌, 값의 범위에 포함되는 행들을 연결하는 방식
+SELECT * FROM SAL_GRADE ;
 
+-- 사원의 급여가
+-- SAL_LEVEL에 작성된 최소(MIN_SAL) ~ 최대(MAX_SAL)
+-- 범위에 맞게 책정되어 있을 때만 조회 결과(RESULT SET)에 포함하기
+
+SELECT 
+	E.EMP_NAME, E.SAL_LEVEL, E.SALARY,
+	S.SAL_LEVEL, S.MIN_SAL, S.MAX_SAL
+FROM
+	EMPLOYEE E
+JOIN
+ 	SAL_GRADE S  
+ 	ON (E.SALARY BETWEEN S.MIN_SAL AND S.MAX_SAL);
+--	SAL_GRADE USING(SAL_LEVEL);
 
 ---------------------------------------------------------------------------------------------------------------
 
@@ -310,13 +324,37 @@ ORDER BY EMP_NAME, DEPT_CODE;
 
 -- 같은 테이블을 조인.
 -- 자기 자신과 조인을 맺음
+--> tip : 같은 테이블이 2개 있다고 생각하면 쉽다!!
+
+-- EMPLOYEE 테이블에서
+-- 모든 사원의 사번, 이름, 사수번호(MANAGER_ID), 사수 이름 조회
 
 -- ANSI 표준
-
+SELECT
+	EMP.EMP_ID, 
+	EMP.EMP_NAME, 
+	NVL(MGR.EMP_ID,'없음') "사수 번호", 
+	NVL(MGR.EMP_NAME,'없음') "사수 이름"
+FROM
+	EMPLOYEE EMP
+LEFT JOIN
+	EMPLOYEE MGR ON (EMP.MANAGER_ID = MGR.EMP_ID)
+ORDER BY
+	EMP.EMP_ID ASC;
 
 
 -- 오라클 구문
-
+SELECT
+	EMP.EMP_ID, 
+	EMP.EMP_NAME, 
+	NVL(MGR.EMP_ID,'없음') "사수 번호", 
+	NVL(MGR.EMP_NAME,'없음') "사수 이름"
+FROM
+	EMPLOYEE EMP, EMPLOYEE MGR
+WHERE
+	EMP.MANAGER_ID = MGR.EMP_ID(+)
+ORDER BY
+	EMP.EMP_ID ASC;
 
 
 ---------------------------------------------------------------------------------------------------------------
@@ -326,20 +364,96 @@ ORDER BY EMP_NAME, DEPT_CODE;
 -- 반드시 두 테이블 간의 동일한 컬럼명, 타입을 가진 컬럼이 필요
 --> 없을 경우 교차조인이 됨.
 
+-- EMPLOYEE 테이블 JOB 테이블 INNER JOIN
+
+-- 1) ON 이용 방법 // 두 테이블 간의 공통 컬럼이 합쳐지지않고 두개 다 나옴
+									 --> 따라서 별칭을 지정해 줘야 한다
+SELECT *
+--	EMP_NAME, E.JOB_CODE, JOB_NAME
+FROM
+	EMPLOYEE E 
+JOIN 
+	JOB J ON (E.JOB_CODE = J.JOB_CODE);
+
+-- 2) USING 이용 방법 //두 테이블 간의 공통 컬럼이 하나로 합쳐짐
+SELECT *
+--	EMP_NAME, JOB_CODE, JOB_NAME
+FROM
+	EMPLOYEE 
+JOIN
+	JOB USING (JOB_CODE);
+
+
+-- NATURAL JOIN 이용
+SELECT
+	EMP_NAME, JOB_CODE, JOB_NAME
+FROM
+	EMPLOYEE 
+NATURAL JOIN JOB;
+-- JOIN 컬럼명 명시 X
+--> 자동으로 같은 타입, 같은 이름의 컬럼을 찾아서 JOIN
+--> 사용자가 테이블의 구조를 모두 파악하고 있어야 사용 가능
+--> 그래서 거의 안씀
+
+/* NATURAL JOIN 실패하는 경우 
+ * -> 같은 타입, 같은 이름의 컬럼이 없을 경우 발생
+ * -> 결과 CROSS JOIN 형태가 조회된다
+ * */
+SELECT
+	EMP_NAME, DEPT_TITLE
+FROM
+	EMPLOYEE 
+NATURAL JOIN 
+	DEPARTMENT ;
+
+
+
 
 ---------------------------------------------------------------------------------------------------------------
 
 -- 7. 다중 조인
 -- N개의 테이블을 조회할 때 사용  (순서 중요!)
 
+-- EMPLOYEE, DEPARTMENT, LOCATION 테이블 JOIN
+
 -- ANSI 표준
+SELECT 
+	EMP_NAME, DEPT_TITLE, LOCAL_NAME
+FROM
+	EMPLOYEE 
+JOIN 
+	DEPARTMENT ON (DEPT_CODE = DEPT_ID) -- 21행 17열
+JOIN 
+	LOCATION ON (LOCATION_ID = LOCAL_CODE); -- 21행 20열
 
+-- 오라클 전용 (AND 구문을 이용해서 추가 JOIN)
+SELECT 
+	EMP_NAME, DEPT_TITLE, LOCAL_NAME
+FROM
+	EMPLOYEE, DEPARTMENT, LOCATION
+WHERE 
+	DEPT_CODE = DEPT_ID
+AND
+	LOCATION_ID = LOCAL_CODE;
 
--- 오라클 전용
-
+	
 
 -- 조인 순서를 지키지 않은 경우(에러발생)
+--> JOIN은 같은 값을 가지는 컬럼을 연결고리 삼아서 
+--  두 테이블을 연결하는 것인데
+-- 	JOIN 순서가 잘못되면 같은 값을 가지는 컬럼이
+--  존재하지 않을 수 있다.
 
+--SELECT 
+--	EMP_NAME, DEPT_TITLE, LOCAL_NAME
+--FROM
+--	EMPLOYEE 
+--JOIN 
+--	LOCATION ON (LOCATION_ID = LOCAL_CODE); -- 21행 20열
+---- EMPLOYEE,LOCATION 각각의 테이블에 서로의 테이블에 존재하는
+---- 공통 컬럼이 존재하지 않음
+--JOIN 
+--	DEPARTMENT ON (DEPT_CODE = DEPT_ID) -- 21행 17열
 
 
 --[다중 조인 연습 문제]
@@ -347,53 +461,184 @@ ORDER BY EMP_NAME, DEPT_CODE;
 -- 직급이 대리이면서 아시아 지역에 근무하는 직원 조회
 -- 사번, 이름, 직급명, 부서명, 근무지역명, 급여를 조회하세요
 
--- ANSI
+SELECT * FROM JOB ; -- 직급
 
+SELECT * FROM LOCATION -- 아시아 지역
+WHERE LOCAL_NAME LIKE 'ASIA%';
+
+SELECT EMP_ID, EMP_NAME 
+FROM EMPLOYEE ; -- 사번, 이름
+
+SELECT  DEPT_ID, DEPT_TITLE
+FROM DEPARTMENT ; -- 부서코드, 부서명
+
+-- ANSI
+SELECT 
+ E.EMP_ID , E.EMP_NAME , J.JOB_NAME , 
+ D.DEPT_TITLE , L.LOCAL_NAME , E.SALARY 
+FROM 
+	EMPLOYEE E
+JOIN
+	JOB J ON (E.JOB_CODE = J.JOB_CODE)
+JOIN 
+	DEPARTMENT D ON (E.DEPT_CODE = D.DEPT_ID)
+JOIN 
+	LOCATION L ON(D.LOCATION_ID = L.LOCAL_CODE)
+WHERE
+	J.JOB_NAME = '대리'
+AND
+	LOCAL_NAME LIKE 'ASIA%';
+	
 -- 오라클 전용
 
 
 
 
 ---------------------------------------------------------------------------------------------------------------
-
+SELECT * FROM EMPLOYEE ;
+SELECT * FROM DEPARTMENT ;
+SELECT * FROM JOB ;
+SELECT * FROM LOCATION ;
+SELECT * FROM SAL_GRADE ;
+SELECT * FROM NATIONAL ;
 
 -- [연습문제]
 
 -- 1. 주민번호가 80년대 생이면서 성별이 여자이고, 성이 '전'씨인 직원들의 
 -- 사원명, 주민번호, 부서명, 직급명을 조회하시오.
-
+SELECT 
+	E.EMP_NAME, E.EMP_NO, DEPT_TITLE, J.JOB_NAME 
+FROM 
+	EMPLOYEE E
+JOIN
+	DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+JOIN
+	JOB J ON (E.JOB_CODE = J.JOB_CODE)
+WHERE 
+	E.EMP_NO LIKE '8%'
+AND 
+	SUBSTR(E.EMP_NO,8,1) = '2'
+AND
+	E.EMP_NAME LIKE '전%';
       
       
 -- 2. 이름에 '형'자가 들어가는 직원들의 사번, 사원명, 부서명을 조회하시오.
-
+SELECT 
+	E.EMP_ID, E.EMP_NAME , J.JOB_NAME 
+FROM 
+	EMPLOYEE E 
+JOIN
+	JOB J ON (E.JOB_CODE = J.JOB_CODE)
+WHERE 
+	E.EMP_NAME LIKE '%형%';
 
 
 -- 3. 해외영업 1부, 2부에 근무하는 사원의 
 -- 사원명, 직급명, 부서코드, 부서명을 조회하시오.
-
-
+SELECT 
+	E.EMP_NAME, J.JOB_NAME, E.DEPT_CODE , DEPT_TITLE 
+FROM 
+	EMPLOYEE E
+JOIN
+	DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+JOIN
+	JOB J ON (E.JOB_CODE = J.JOB_CODE)
+WHERE
+	DEPT_TITLE IN ('해외영업1부','해외영업2부')
+ORDER BY
+	DEPT_TITLE DESC;
+	
+	
 --4. 보너스포인트를 받는 직원들의 사원명, 보너스포인트, 부서명, 근무지역명을 조회하시오.
-
+SELECT 
+	EMP_NAME, BONUS, DEPT_TITLE, LOCAL_NAME
+FROM 
+	EMPLOYEE 
+JOIN
+	DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+JOIN 
+	LOCATION ON (LOCATION_ID = LOCAL_CODE)
+WHERE 
+	BONUS IS NOT NULL;
+	
 
 --5. 부서가 있는 사원의 사원명, 직급명, 부서명, 지역명 조회
+SELECT 
+ E.EMP_NAME, J.JOB_NAME , DEPT_TITLE , LOCAL_NAME
+FROM 
+	EMPLOYEE E
+JOIN
+	DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+JOIN 
+	LOCATION ON (LOCATION_ID = LOCAL_CODE)
+JOIN
+	JOB J ON (E.JOB_CODE = J.JOB_CODE)
+WHERE 
+	DEPT_TITLE IS NOT NULL;
 
 
 -- 6. 급여등급별 최소급여(MIN_SAL)를 초과해서 받는 직원들의
 --사원명, 직급명, 급여, 연봉(보너스포함)을 조회하시오.
 --연봉에 보너스포인트를 적용하시오.
-
+SELECT 
+	E.EMP_NAME , J.JOB_NAME , E.SALARY , E.SALARY*(1+NVL(E.BONUS,0)) *12 "연봉"
+FROM 
+	EMPLOYEE E
+JOIN
+	JOB J ON (E.JOB_CODE = J.JOB_CODE)
+JOIN
+ 	SAL_GRADE S ON (E.SAL_LEVEL = S.SAL_LEVEL)
+WHERE
+	E.SALARY > S.MIN_SAL;
 
 
 -- 7.한국(KO)과 일본(JP)에 근무하는 직원들의 
 -- 사원명, 부서명, 지역명, 국가명을 조회하시오.
+SELECT 
+	EMP_NAME "사원명", DEPT_TITLE "부서명", 
+	L.LOCAL_NAME "지역명", N.NATIONAL_NAME "국가명"
+FROM 
+	EMPLOYEE E
+JOIN
+	DEPARTMENT D ON (E.DEPT_CODE = D.DEPT_ID)
+JOIN 
+	LOCATION L ON (D.LOCATION_ID = L.LOCAL_CODE)
+JOIN
+	NATIONAL N ON (N.NATIONAL_CODE = L.NATIONAL_CODE)
+WHERE
+	N.NATIONAL_NAME IN ('한국','일본');
 
 
 
 -- 8. 같은 부서에 근무하는 직원들의 사원명, 부서코드, 동료이름을 조회하시오.
 -- SELF JOIN 사용
-
+SELECT
+	E.EMP_NAME , E.DEPT_CODE, F.EMP_NAME
+FROM 
+	EMPLOYEE E
+JOIN
+	EMPLOYEE F ON (E.DEPT_CODE = F.DEPT_CODE)
+WHERE 
+	E.EMP_NAME != F.EMP_NAME
+ORDER BY
+	E.EMP_NAME ASC;
 
 
 -- 9. 보너스포인트가 없는 직원들 중에서 직급코드가 J4와 J7인 직원들의 사원명, 직급명, 급여를 조회하시오.
 -- 단, JOIN, IN 사용할 것
+SELECT
+	E.EMP_NAME, J.JOB_NAME, E.SALARY 
+FROM 
+	EMPLOYEE E
+JOIN
+	JOB J ON (E.JOB_CODE = J.JOB_CODE)
+WHERE
+	BONUS IS NULL
+AND
+	E.JOB_CODE IN ('J4','J7');
+
+
+
+
+
 
--- General/Scripts/WORKBOOK_01_BASIC.sql
+++ General/Scripts/WORKBOOK_01_BASIC.sql
@@ -183,12 +183,19 @@ FROM
 WHERE
 	--(TO_NUMBER(SUBSTR(ENTRANCE_DATE,1,4)) 
 	EXTRACT(YEAR FROM ENTRANCE_DATE) - 
-	TO_NUMBER('19'|| SUBSTR(STUDENT_SSN,1,2))+1) > 19; 
+	TO_NUMBER('19'|| SUBSTR(STUDENT_SSN,1,2)) > 19; 
+
 
 
 -- 6번
 -- 춘 기술대학교의 2000년도 이후 입학자들은 학번이 A로 시작하게 되어있다.
 -- 2000년도 이전 학번을 받은 학생들의 학번과 이름 조회하는 SQL을 작성하시오.
+SELECT 
+	STUDENT_NO, STUDENT_NAME
+FROM 	
+	TB_STUDENT 
+WHERE 
+	NOT STUDENT_NO LIKE 'A%';
 
 
 
@@ -196,31 +203,69 @@ WHERE
 -- 학번이 A517178인 한아름 학생의 학점 총 평점을 구하는 SQL문을 작성하시오.
 -- 단, 이때 출력 화면의 헤더는 "평점"이라고 찍히게 하고,
 -- 점수는 반올림하여 소수점 이하 한자리까지만 표시한다.
+SELECT 
+	ROUND(SUM(POINT)/COUNT(POINT),1)
+FROM 
+	TB_GRADE G
+JOIN
+	TB_STUDENT S ON (S.STUDENT_NO = G.STUDENT_NO)
+WHERE 
+	S.STUDENT_NO = 'A517178';
 
 
 
 -- 8번
 -- 학과별 학생 수를 구하여 "학과번호", "학생수(명)"의 형태로 조회하시오.
+SELECT 
+	DEPARTMENT_NO AS "학과번호", COUNT(STUDENT_NO) AS "학생수(명)"
+FROM 
+	TB_STUDENT 
+GROUP BY
+	DEPARTMENT_NO
+ORDER BY
+	DEPARTMENT_NO ASC;
 
 
 
 -- 9번
 -- 지도 교수를 배정받지 못한 학생의 수를 조회하시오.
+SELECT 
+	COUNT(*)
+FROM 	
+	TB_STUDENT 
+WHERE 
+	COACH_PROFESSOR_NO IS NULL;
 
-
+SELECT * FROM TB_GRADE ;
 
 -- 10번
 -- 학번이 A112113인 김고운 학생의 년도 별 평점을 구하는 SQL문을 작성하시오.
 -- 단, 이때 출력화면의 헤더는 "년도", "년도 별 평점"이라고 찍히게 하고,
 -- 점수는 반올림하여 소수점 이하 한자리까지만 표시한다.
-
-
-
+SELECT 
+	SUBSTR(TERM_NO,1,4) AS "년도", ROUND(SUM(POINT)/COUNT(POINT),1) AS "년도 별 평점"
+FROM 
+	TB_GRADE 
+WHERE 
+	STUDENT_NO = 'A112113'
+GROUP BY
+	SUBSTR(TERM_NO,1,4)
+ORDER BY
+	SUBSTR(TERM_NO,1,4) ASC;
 
 -- 11번
 -- 학과 별 휴학생 수를 파악하고자 한다.
 -- 학과 번호와 휴학생 수를 조회하는 SQL을 작성하시오.
-
+--SELECT 
+--	DEPARTMENT_NO AS "학과코드명" , 
+--
+--	COUNT(STUDENT_NO) AS "휴학생 수"
+--FROM 
+--	TB_STUDENT 
+--GROUP BY
+--	DEPARTMENT_NO 
+--ORDER BY 
+--	DEPARTMENT_NO ASC;
 
 
 
@@ -229,7 +274,10 @@ WHERE
 
 
 
-
+- 13번
+-- 학번이 A112113인 김고운 학생의 학점을 조회하려고 한다.
+-- 년도, 학기 별 평점과 년도 별 누적 평점, 총 평점을 구하는 SQL을 작성하시오.
+-- (단, 평점은 소수점 1자리까지만 반올림하여 표시한다.)
 
 
 
