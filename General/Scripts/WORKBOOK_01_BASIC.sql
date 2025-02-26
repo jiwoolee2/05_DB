b3247c580dccd75f563ff26e411d2c5d333db1b9
jiwoolee2 <dlwldn1009@naver.com>
2025-02-26 17:37:03 +0900

    0226

--- General/Scripts/WORKBOOK_01_BASIC.sql
+++ General/Scripts/WORKBOOK_01_BASIC.sql
@@ -258,15 +258,13 @@ ORDER BY
 -- 학과 번호와 휴학생 수를 조회하는 SQL을 작성하시오.
 SELECT 
 	DEPARTMENT_NO AS "학과코드명" , 
-	DECODE(DEPARTMENT_NO, EXISTS(  AS "휴학생 수"
+	COUNT(DECODE(ABSENCE_YN,'Y','휴학생')) AS "휴학생 수"
 FROM 
 	TB_STUDENT 
 GROUP BY
 	DEPARTMENT_NO 
--- HAVING
--- ABSENCE_YN = 'Y'
 ORDER BY 
-	"학과코드명" ASC;
+	DEPARTMENT_NO ASC;
 
 
 
@@ -275,15 +273,25 @@ ORDER BY
 
 
 
+
+
+
 -- 13번
 -- 학번이 A112113인 김고운 학생의 학점을 조회하려고 한다.
 -- 년도, 학기 별 평점과 년도 별 누적 평점, 총 평점을 구하는 SQL을 작성하시오.
 -- (단, 평점은 소수점 1자리까지만 반올림하여 표시한다.)
-
-
-
-
-
+SELECT 
+	NVL(SUBSTR(TERM_NO,1,4),' ') AS "년도" , 
+	NVL(SUBSTR(TERM_NO,5),' ') AS "학기" , 
+	ROUND(AVG(POINT),1) 평점
+FROM
+	TB_GRADE 
+WHERE 
+	STUDENT_NO = 'A112113'
+GROUP BY
+	ROLLUP(SUBSTR(TERM_NO,1,4),SUBSTR(TERM_NO,5))
+ORDER BY
+	SUBSTR(TERM_NO,1,4) ASC;
 
 
 
