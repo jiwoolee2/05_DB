ed772fc39b2234499eba263f49271d2db1e7e570
이지우 <127360928+jiwoolee2@users.noreply.github.com>
2025-04-04 15:53:30 +0900

    0404

---
+++ General/Scripts/insertClothes.sql
@@ -0,0 +1,38 @@
+INSERT INTO PRODUCT_INFO VALUES('MP0000001', '와이드 데님 팬츠', 'pants', 'men', 89000, '/images/men/men-pants1.png');
+INSERT INTO PRODUCT_INFO VALUES('MP0000002', '연청 루즈진', 'pants', 'men', 92000, '/images/men/men-pants2.png');
+INSERT INTO PRODUCT_INFO VALUES('MP0000003', '빈티지 워싱 데님', 'pants', 'men', 95000, '/images/men/men-pants3.png');
+INSERT INTO PRODUCT_INFO VALUES('MP0000004', '워싱 크랙 데님 팬츠', 'pants', 'men', 112000, '/images/men/men-pants4.png');
+INSERT INTO PRODUCT_INFO VALUES('MP0000005', '블랙 슬랙스 팬츠', 'pants', 'men', 130000, '/images/men/men-pants5.png');
+INSERT INTO PRODUCT_INFO VALUES('MP0000006', '오버핏 조거팬츠', 'pants', 'men', 78000, '/images/men/men-pants6.png');
+INSERT INTO PRODUCT_INFO VALUES('MP0000007', '트랙 라인 팬츠', 'pants', 'men', 84000, '/images/men/men-pants7.png');
+INSERT INTO PRODUCT_INFO VALUES('MP0000008', '카고 와이드 팬츠', 'pants', 'men', 87000, '/images/men/men-pants8.png');
+INSERT INTO PRODUCT_INFO VALUES('MP0000009', '스트링 조거 카고', 'pants', 'men', 92000, '/images/men/men-pants9.png');
+INSERT INTO PRODUCT_INFO VALUES('MP0000010', '블랙 스트레이트 데님', 'pants', 'men', 97000, '/images/men/men-pants10.png');
+
+INSERT INTO PRODUCT_INFO VALUES('MS0000001', '카키 체크 플란넬 셔츠', 'shirt', 'men', 89000, '/images/men/men-shirt1.png');
+INSERT INTO PRODUCT_INFO VALUES('MS0000002', '화이트 옥스포드 셔츠', 'shirt', 'men', 92000, '/images/men/men-shirt2.png');
+INSERT INTO PRODUCT_INFO VALUES('MS0000003', '네이비 무지 셔츠', 'shirt', 'men', 95000, '/images/men/men-shirt3.png');
+INSERT INTO PRODUCT_INFO VALUES('MS0000004', '연베이지 블루 체크 셔츠', 'shirt', 'men', 112000, '/images/men/men-shirt4.png');
+INSERT INTO PRODUCT_INFO VALUES('MS0000005', '블랙 그레이 체크 셔츠', 'shirt', 'men', 130000, '/images/men/men-shirt5.png');
+INSERT INTO PRODUCT_INFO VALUES('MS0000006', '그린 깅엄 체크 셔츠', 'shirt', 'men', 78000, '/images/men/men-shirt6.png');
+INSERT INTO PRODUCT_INFO VALUES('MS0000007', '스카이블루 체크 셔츠', 'shirt', 'men', 84000, '/images/men/men-shirt7.png');
+INSERT INTO PRODUCT_INFO VALUES('MS0000008', '브라운 블루 체크 셔츠', 'shirt', 'men', 87000, '/images/men/men-shirt8.png');
+INSERT INTO PRODUCT_INFO VALUES('MS0000009', '베이지 깅엄 체크 셔츠', 'shirt', 'men', 92000, '/images/men/men-shirt9.png');
+INSERT INTO PRODUCT_INFO VALUES('MS0000010', '블루 그레이 체크 셔츠', 'shirt', 'men', 97000, '/images/men/men-shirt10.png');
+
+INSERT INTO PRODUCT_INFO VALUES('MSTW0000001', '카키 컬러 맨투맨', 'sweat', 'men', 69000, '/images/men/men-sweat1.png');
+INSERT INTO PRODUCT_INFO VALUES('MSTW0000002', '화이트 베이직 맨투맨', 'sweat', 'men', 72000, '/images/men/men-sweat2.png');
+INSERT INTO PRODUCT_INFO VALUES('MSTW0000003', '딥네이비 무지 맨투맨', 'sweat', 'men', 78000, '/images/men/men-sweat3.png');
+INSERT INTO PRODUCT_INFO VALUES('MSTW0000004', '라이트블루 루즈핏 맨투맨', 'sweat', 'men', 85000, '/images/men/men-sweat4.png');
+INSERT INTO PRODUCT_INFO VALUES('MSTW0000005', '블랙 오버핏 맨투맨', 'sweat', 'men', 93000, '/images/men/men-sweat5.png');
+INSERT INTO PRODUCT_INFO VALUES('MSTW0000006', '빈티지 민트 맨투맨', 'sweat', 'men', 74000, '/images/men/men-sweat6.png');
+INSERT INTO PRODUCT_INFO VALUES('MSTW0000007', '스카이블루 프린팅 맨투맨', 'sweat', 'men', 77000, '/images/men/men-sweat7.png');
+INSERT INTO PRODUCT_INFO VALUES('MSTW0000008', '브라운 워싱 맨투맨', 'sweat', 'men', 79000, '/images/men/men-sweat8.png');
+INSERT INTO PRODUCT_INFO VALUES('MSTW0000009', '코튼 베이지 맨투맨', 'sweat', 'men', 81000, '/images/men/men-sweat9.png');
+INSERT INTO PRODUCT_INFO VALUES('MSTW0000010', '차콜 블루믹스 맨투맨', 'sweat', 'men', 86000, '/images/men/men-sweat10.png');
+
+INSERT INTO PRODUCT_INFO VALUES('MOW0000008', '브라운 워싱 자켓', 'outer', 'men', 79000, '/images/men/men-outer1.png');
+INSERT INTO PRODUCT_INFO VALUES('MOW0000009', '코튼 베이지 자켓', 'outer', 'men', 81000, '/images/men/men-outer2.png');
+INSERT INTO PRODUCT_INFO VALUES('MOW0000010', '차콜 블루믹스 자켓', 'outer', 'men', 86000, '/images/men/men-outer3.png');
+
+COMMIT;
